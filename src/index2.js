70b3846823b492f8ac597f58b1429315d1fca215
ofer shaham <121024880+ofer-shaham@users.noreply.github.com>
2024-03-12 21:54:07 +0200

    add alternative recognition method

--- src/index2.js
+++ src/index2.js
@@ -120,6 +120,63 @@ function analyzeText(speechResult) {
 //     speechRecognitionList.addFromString(grammar, 1);
 //     return  speechRecognitionList;
 //  }
+function option1(){
+    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechGrammarList = SpeechGrammarList || window.webkitSpeechGrammarList
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
+var colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
+
+var recognition = new SpeechRecognition();
+if (SpeechGrammarList) {
+  // SpeechGrammarList is not currently available in Safari, and does not have any effect in any other browser.
+  // This code is provided as a demonstration of possible capability. You may choose not to use it.
+  var speechRecognitionList = new SpeechGrammarList();
+  var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
+  speechRecognitionList.addFromString(grammar, 1);
+  recognition.grammars = speechRecognitionList;
+}
+recognition.continuous = false;
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+
+recognition.onresult = function(event) {
+  // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
+  // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
+  // It has a getter so it can be accessed like an array
+  // The first [0] returns the SpeechRecognitionResult at the last position.
+  // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
+  // These also have getters so they can be accessed like arrays.
+  // The second [0] returns the SpeechRecognitionAlternative at position 0.
+  // We then return the transcript property of the SpeechRecognitionAlternative object
+  const color = event.results[0][0].transcript;
+console.log(color)
+  // diagnostic.textContent = 'Result received: ' + color + '.';
+  // bg.style.backgroundColor = color;
+  console.log('Confidence: ' + event.results[0][0].confidence);
+}
+
+recognition.onspeechend = function() {
+  recognition.stop();
+}
+
+recognition.onnomatch = function(event) {
+console.log(
+  "I didn't recognise that color.";  
+) 
+}
+
+recognition.onerror = function(event) {
+console.log(
+  'Error occurred in recognition: ' + event.error;  
+) 
+}
+}
+function option2(){
+    
+
 navigator.mediaDevices.getUserMedia({
     audio: true
 }).then(function(stream) {
@@ -257,3 +314,6 @@ navigator.mediaDevices.getUserMedia({
 }).catch(function(error) {
     console.log("Error accessing microphone: " + error);
 });
+}
+
+option1()
